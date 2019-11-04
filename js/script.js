9975f7b13f32ff5cbdf9e95587a9b7d88e34fc09
abdulmajid shire <jido@abdulazizs-MacBook-Air.local>
2019-11-04 17:37:11 +0300

    push gh-pages

--- js/script.js
+++ js/script.js
@@ -33,35 +33,4 @@ $(document).ready(function () {
 
 
 
-
-
-
-
-    // $(".mytext").hide();
-    // $(".mytext1").hide();
-    // $(".mytext2").hide();
-    // $(".mytext3").hide();
-    // $(".mytext4").hide();
-    // $(".mytext5").hide();
-    // $(".mytext6").hide();
-    // $(".mytext7").hide();
-    // $("hoverme").mouseenter(function(){
-    //     $(".hoverme").addClass("hover1");
-    //     $(".mytext").show();
-    // })
-    // $("hoverme").mouseleave(function(){
-    //     $(".hoverme").removeClass("hover1");
-    //     $(".mytext").hide();
-    // })
-    // $(".hoverme,.hoverme1,.hoverme2,.hoverme3,.hoverme4,.hoverme5,.hoverme6,.hoverme7").mouseleave(function () {
-    //     $(".hoverme").removeClass("hover1");
-    //     $(".hoverme1").removeClass("hover2");
-    //     $(".hoverme2").removeClass("hover3");
-    //     $(".hoverme3").removeClass("hover4");
-    //     $(".hoverme4").removeClass("hover5");
-    //     $(".hoverme5").removeClass("hover6");
-    //     $(".hoverme6").removeClass("hover7");
-    //     $(".hoverme7").removeClass("hover8");
-    //     $(".mytext").hide();
-    // })
-//})
+    
